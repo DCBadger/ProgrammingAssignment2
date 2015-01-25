0395661ec0e47fe850b717cc93e66a85036fe8ba
Sam Nitz <samuelnitz@gmail.com>
2015-01-25 15:10:35 -0500

    Sam Nitz edits for PA2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+##This function gets/sets the value of a matrix, then gets/sets the inversion of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
+			m <- NULL  
+            set <- function(y) { 
+                    x <<- y
+                    m <<- NULL
+            }
+            get <- function() x 
+            setmatrix <- function(solve) m <<- solve 
+            getmatrix <- function() m 
+            list(set = set, get = get,
+                 setmatrix = setmatrix,
+                 getmatrix = getmatrix) 
 }
 
 
-## Write a short comment describing this function
+## This function checks to see if there's a cached value of m and returns that value, if not it calculates the inversiongit
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		m <- x$getmatrix() ##gets the value of the matrix for x 
+            if(!is.null(m)) { ##if a cached matrix exists, returns that value
+                    message("getting cached data")
+                    return(m)
+            }
+            matrix <- x$get() 
+            m <- solve(matrix, ...) ##inverts the matrix
+            x$setmatrix(m) 
+            m 
+        
 }
